7209278bae6264a0f89eab91f4e8ed1892673524
Léo OTSHUDI <leootshudi@gmail.com>
2022-10-17 18:42:03 +0200

    frontend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const NavBar = () => {
     return (
         <div className="text-white">
-            [Navbar here]
+            <nav className="flex justify-between mx-8 p-8">
+                <div>
+                    <h1 className="text-4xl font-bold">ACE</h1>
+                </div>
+                <div>
+                    <button className="rounded-md bg-white text-black px-6 py-2">Connect wallet</button>
+                </div>
+            </nav>
         </div>
     )
 }
