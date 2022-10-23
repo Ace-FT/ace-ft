122eef7f0705b8c362425dce0f1319fffbf757ac
Léo OTSHUDI <leootshudi@gmail.com>
2022-10-23 15:55:24 +0200

    Reorganisation + Navigation routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, {useContext} from 'react';
+import { NavLink } from 'react-router-dom';
 import { AceContext } from '../context/context';
 import { shortenAddress } from '../utils/shortenAddress';
 
@@ -10,28 +11,33 @@ const NavBar = () => {
       <div className="text-white">
         <nav className="flex justify-between p-8">
           <div>
-            <h1 className="text-4xl font-bold">ACE</h1>
+            <h1 className="text-4xl font-bold">
+              <NavLink to="/">ACE</NavLink>
+            </h1>
           </div>
           {connectedAccount ? (
             <div className="flex items-center">
-              <button
+              <NavLink
                 className="rounded-l-md border-r border-gray-200 bg-white text-black px-6 py-2"
-                onClick={() => {}}
+                activeclassname="is-active"
+                to="/inbox"
               >
                 My inbox
-              </button>
-              <button
+              </NavLink>
+              <NavLink
                 className="border-r border-gray-200 bg-white text-black px-6 py-2"
-                onClick={() => {}}
+                activeclassname="is-active"
+                to="/sent"
               >
                 Sent items
-              </button>
-              <button
+              </NavLink>
+              <NavLink
                 className="rounded-r-md bg-white text-black px-6 py-2"
-                onClick={() => {}}
+                activeclassname="is-active"
+                to="/settings"
               >
                 Settings
-              </button>
+              </NavLink>
               <p className="mx-8">Hello! {shortenAddress(connectedAccount)} 👋 </p>
             </div>
           ) : (
