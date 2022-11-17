3791a89deb3aaf613cfe0cba6addaa611c9ff769
Léo OTSHUDI <leootshudi@gmail.com>
2022-11-17 22:25:08 +0100

    Telegram redirection

--- src/components/PersonnalDataForm.jsx
+++ src/components/PersonnalDataForm.jsx
@@ -2,30 +2,33 @@ import React, { useContext } from "react";
 import {Link} from "react-router-dom";
 
 const PersonnalDataForm = () => {
+
   return (
     <>
       <form
-        action="post"
         className="flex flex-col max-w-1/2 justify-center mx-auto"
       >
         <div className="flex w-96 items-center px-4 py-4">
           <label htmlFor="profpic" className="w-32 mr-2">
             Profile picture
           </label>
-          <input
+          {/* <input
             type="text"
             name="profpic"
             id="profpic"
             className="w-full rounded-md border border-gray-500 focus:outline-none ml-2 p-2"
-          />
+          /> */}
         </div>
         <div className="flex w-96 items-center px-4 py-4">
           <p className="w-32 mr-2">Telegram</p>
-          <a href="https://t.me/ace_ft_bot">
-            <button className="w-full rounded-md bg-blue-500 text-white focus:outline-none ml-2 p-2">
-              Connect telegram
-            </button>
-          </a>
+              <button
+                className="w-full rounded-md bg-blue-500 text-white focus:outline-none ml-2 p-2" 
+                onClick={() => 
+                  window.open("https://t.me/ace_ft_bot")
+                }
+              >
+                Connect telegram
+              </button>
         </div>
       </form>
     </>
