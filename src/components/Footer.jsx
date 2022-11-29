05e4119474726be91534023aa96ec45d6a9302cc
Léo OTSHUDI <leootshudi@gmail.com>
2022-11-29 11:44:23 +0100

    Footer position

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
       <div className="flex items-center justify-center">
         <div className="footer-container">
           <footer>
-            <div className="justify-right ml-auto flex basis-1/3">
-              <div className="ml-auto mr-0 flex items-center font-logo text-xs">
-                <p className="mr-2 text-right">Powered by iExec</p>
-                <img src={require("../assets/cropped-favicon-192x192.png")} alt="iExec logo" className="h-4" />
+            <div className="justify-center mx-auto flex basis-1/3">
+              <div className="mx-auto flex items-center font-logo text-base">
+                <p className="mx-2 text-center">Powered by iExec</p>
+                <img src={require("../assets/cropped-favicon-192x192.png")} alt="iExec logo" className="h-5" />
               </div>
             </div>
           </footer>
