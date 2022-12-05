8999f7c2dbfa1a3b95091a1fba86b354753ffb1c
Léo OTSHUDI <leootshudi@gmail.com>
2022-12-05 11:36:29 +0100

    refresh on wallet connection changed

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Helmet from "react-helmet";
 
 function App() {
   const { connectedAccount, connectWallet, bgUrls, background, creativeMode, setCreativeMode } = useContext(AceContext);
+  const { ethereum } = window;
 
   useEffect(() => {
     const connectWalletOnPageLoad = async () => {
@@ -37,7 +38,19 @@ function App() {
     console.log(creativeMode);
   }, [creativeMode]);
 
+  useEffect(() => {
+    refreshOnWalletChange()
+  }, [])
+
   const isConnected = connectedAccount !== "";
+
+  const refreshOnWalletChange = () => {
+    if(ethereum) {
+      ethereum.on('accountsChanged', function() {
+        window.location.reload()
+      })
+    }
+  }
 
   return (
     <div
