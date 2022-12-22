db8fe7affdfbd3d1843b79b6121fcdecf072803b
m0rpheus <fotshudi@gmail.com>
2022-12-22 12:53:55 +0100

    Update constants.js

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,5 +1,5 @@
 export const SMS_URL = "https://v7.sms.debug-tee-services.bellecour.iex.ec";
-export const APP_ADDRESS = "0xFBA05EA37e31cB1776AE13880DACD0336B7cF113";
+export const APP_ADDRESS = "0x2F33B92E52ea1B821F451C5eD692A158aE1452B4";
 export const WORKERPOOL_ADDRESS = "0x9849e7496cdbff132c84753591d09b181c25f29a";
 export const TEE_TAG = "0x0000000000000000000000000000000000000000000000000000000000000001";
 export const STEP_COMPLETED = "Your file is uploaded ✅";
